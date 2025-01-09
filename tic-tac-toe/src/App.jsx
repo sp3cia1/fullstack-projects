5e42f3f0ba1dbca09fae5ef98e50e850608ec911
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-01-09 20:51:58 +0530

    rendered and styled the board and cells

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,9 +1,17 @@
+import Cell from "./components/cell"
 
 function App() {
+  let cells = [1,2,3,4,5,6,7,8,9]
 
   return (
     <>
-      
+      <div className="board">
+        {
+          cells.map((id) => (
+            <Cell key = {id} id = {id}/>
+          ))
+        }
+      </div>
     </>
   )
 }
---
+++ tic-tac-toe/src/components/Cell.jsx
@@ -0,0 +1,9 @@
+function Cell(props){
+    const { id } = props;
+
+    return(
+        <div className="cell"></div>
+    )
+}
+
+export default Cell;
