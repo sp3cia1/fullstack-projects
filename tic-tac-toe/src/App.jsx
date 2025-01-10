743b5b68212b6467d44822437a09d57e976d2770
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-01-10 15:29:05 +0530

    completed the winning conditions checks

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,7 +2,14 @@ import { useState } from "react"
 import Cell from "./components/cell"
 
 function App() {
-  let cells = [1,2,3,4,5,6,7,8,9]
+  const cells = [1,2,3,4,5,6,7,8,9]
+
+  const necessaryNums = [3,5,7] //every winning combination includes these numbers
+  const winningCombinations =[
+    [3,5,7],[1,5,7],
+    [1,2,3],[4,5,6],[7,8,9],
+    [3,6,9],[2,5,8],[1,4,7]
+  ]
 
   const [currentPlayer, setCurrentPlayer] = useState('p1')
   const [p1Cells, setP1Cells] = useState([])
@@ -10,15 +17,29 @@ function App() {
 
 
   function handleClick(id) {
-    console.log(p1Cells)
     if(currentPlayer == 'p1'){
-      setCurrentPlayer('p2')
       const updatedP1Cells = [...p1Cells,id];
       setP1Cells(updatedP1Cells)
+      checkWin(updatedP1Cells)
+      setCurrentPlayer('p2')
     } else {
-      setCurrentPlayer('p1')
       const updatedP2Cells = [...p2Cells,id];
       setP2Cells(updatedP2Cells)
+      checkWin(updatedP2Cells)
+      setCurrentPlayer('p1')
+    }
+  }
+
+  //helper function to check for winning combos
+  const hasWinningCombo = (playerMoves, winningCombos) => {
+    return winningCombos.some(combo => combo.every(num => playerMoves.includes(num)));
+  };
+
+  function checkWin(playerCells){
+    if(playerCells.length > 2 && playerCells.some(num => necessaryNums.includes(num)) ) {
+      if(hasWinningCombo(playerCells, winningCombinations)){
+        console.log(`${currentPlayer} WON`)
+      }
     }
   }
 
