b1cea088d136a670dce23f6af5c63b8d10305ab4
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-01-10 17:13:54 +0530

    added and styled a info board and a restart button

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,32 +1,36 @@
 import { useState } from "react"
 import Cell from "./components/cell"
+import InfoBoard from "./components/InfoBoard"
 
 function App() {
   const cells = [1,2,3,4,5,6,7,8,9]
 
   const necessaryNums = [3,5,7] //every winning combination includes these numbers
   const winningCombinations =[
-    [3,5,7],[1,5,7],
+    [3,5,7],[1,5,9],
     [1,2,3],[4,5,6],[7,8,9],
     [3,6,9],[2,5,8],[1,4,7]
   ]
 
   const [currentPlayer, setCurrentPlayer] = useState('p1')
+  const [gameFinished, setGameFinished] = useState(false)
   const [p1Cells, setP1Cells] = useState([])
   const [p2Cells, setP2Cells] = useState([])
 
 
   function handleClick(id) {
-    if(currentPlayer == 'p1'){
-      const updatedP1Cells = [...p1Cells,id];
-      setP1Cells(updatedP1Cells)
-      checkWin(updatedP1Cells)
-      setCurrentPlayer('p2')
-    } else {
-      const updatedP2Cells = [...p2Cells,id];
-      setP2Cells(updatedP2Cells)
-      checkWin(updatedP2Cells)
-      setCurrentPlayer('p1')
+    if (!gameFinished){
+      if(currentPlayer == 'p1'){
+        const updatedP1Cells = [...p1Cells,id];
+        setP1Cells(updatedP1Cells)
+        checkWin(updatedP1Cells)
+        setCurrentPlayer('p2')
+      } else {
+        const updatedP2Cells = [...p2Cells,id];
+        setP2Cells(updatedP2Cells)
+        checkWin(updatedP2Cells)
+        setCurrentPlayer('p1')
+      }
     }
   }
 
@@ -38,14 +42,25 @@ function App() {
   function checkWin(playerCells){
     if(playerCells.length > 2 && playerCells.some(num => necessaryNums.includes(num)) ) {
       if(hasWinningCombo(playerCells, winningCombinations)){
-        console.log(`${currentPlayer} WON`)
+        console.log(`${currentPlayer} WON, player moves ${playerCells}`)
+
+        setGameFinished(prev => !prev)
       }
     }
   }
 
+  function handleRestart(){
+    setP1Cells([])
+    setP2Cells([])
+    setCurrentPlayer('p1')
+    setGameFinished(false)
+  }
+
   return (
     <>
+      <InfoBoard gameFinished = { gameFinished } currentPlayer = { currentPlayer } handleRestart = {handleRestart}/>
       <div className="board">
+        
         {
           cells.map((id) => (
             <Cell 
