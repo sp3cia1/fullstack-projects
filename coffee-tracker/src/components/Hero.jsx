20501d710a4c0251e91556e6707a17e3046c9d6f
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-01-11 20:55:13 +0530

    Completed the hero section, the header and the footer

--- coffee-tracker/src/components/Hero.jsx
+++ coffee-tracker/src/components/Hero.jsx
@@ -1,7 +1,26 @@
 export default function Hero(){
     return(
         <>
-        
+            <h1>Coffee Tracking for Coffee <abbr title="An enthusiast or devotee">Fiends</abbr></h1>
+
+            <div className="benefits-list">
+                <h3 className="font-bolder">
+                    Try <span className="text-gradient">Caffiend</span> and start ...
+                </h3>
+                <p>✅ Tracking every Coffee</p>
+                <p>✅ Measuring your bloof Caffeine levels</p>
+                <p>✅ Costing and quantifying your Addiction</p>
+            </div>
+            
+            <div className="card info-card">
+                <div>
+                    <i className="fa-solid fa-circle-info"></i>
+                    <h3>Did you know...</h3>
+                </div>
+                <h5>That Caffeine&apos;s half-life is about 5 hours?</h5>
+                <p>This means that after 5 hours, half the caffeine you consumed is still in your system, keeping you alert longer! So if you drink a cup of coffee with 200 mg of caffeine, 5 hours, later, you&apos;ll still have about 100 mg of caffeine in your system.</p>
+            </div>
+            
         </>
     )
 }
--- coffee-tracker/src/components/Layout.jsx
+++ coffee-tracker/src/components/Layout.jsx
@@ -3,12 +3,21 @@ export default function Layout(props) {
 
     const header = (
         <header>
-
+            <div>
+                <h1 className="text-gradient">Coffee-Tracker</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
         </header>
     )
 
     const footer = (
-        <footer></footer>
+        <footer>
+            <p>This <span>Coffee Tracker</span> was made by <a href="https://github.com/sp3cia1">Pranjal</a></p>
+        </footer>
     )
 
     return(
@@ -18,7 +27,6 @@ export default function Layout(props) {
                 {children}
             </main>
             {footer}
-        
         </>
     )
 }
