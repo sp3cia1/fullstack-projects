2d0e2759be1bcfbf6856ba8616c51fc5751d0faa
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-01-13 17:04:48 +0530

    completed the history component

--- coffee-tracker/src/components/History.jsx
+++ coffee-tracker/src/components/History.jsx
@@ -1,7 +1,31 @@
+import { calculateCoffeeStats, calculateCurrentCaffeineLevel, coffeeConsumptionHistory, getCaffeineAmount, timeSinceConsumption } from "../utils";
+
 export default function History(){
     return(
         <>
-        
+            <div className="section-header">
+                <i className="fa-solid fa-time"></i>
+                <h2>History</h2>
+            </div>
+            <p><i>Hover for more information!</i></p>
+            <div className="coffee-history">
+                {Object.keys(coffeeConsumptionHistory).sort((a,b) => b-a).map((utcTime, coffeeIndex) => {
+                        const coffee = coffeeConsumptionHistory[utcTime]
+                        const timeSinceConsume = timeSinceConsumption(utcTime)
+                        const originalAmount = getCaffeineAmount(coffee.name)
+                        const remainingAmount = calculateCurrentCaffeineLevel({
+                            [utcTime]: coffee
+                        })
+
+                        const summary = `${coffee.name} | $${coffee.cost} | ${timeSinceConsume} | ${originalAmount}mg / ${remainingAmount}mg`
+
+                        return (
+                            <div title = {summary} key = {coffeeIndex}>
+                                <i className="fa-solid fa-mug-hot"/>
+                            </div>
+                        )
+                })}
+            </div>
         </>
     )
 }
