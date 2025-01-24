f716e8caac6e7d84d3771fac5044b106d70b182c
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-01-24 16:07:00 +0530

    created the login component

--- realtime-cursors/client/src/App.jsx
+++ realtime-cursors/client/src/App.jsx
@@ -1,9 +1,10 @@
-
+import Login from "./Login/Login.jsx"
 
 function App() {
 
   return (
     <>
+      <Login/>
     </>
   )
 }
---
+++ realtime-cursors/client/src/Login/Login.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function Login({ onSubmit }){
+    const [username, setUsername] = useState('');
+    console.log(username)
+    return(
+        <div>
+            <h1>Welcome</h1>
+            <p>What Should people call you ?</p>
+            <form 
+                onSubmit={(e) =>{
+                    e.preventDefault()
+                    onSubmit(username)
+                }}
+            >
+                <input 
+                    type="text" 
+                    value={username}
+                    placeholder="username"
+                    onChange={(e) => setUsername(e.target.value)} 
+                />
+                <input type="submit" />
+            </form>
+        </div>
+    )
+}
+
+export default Login;
