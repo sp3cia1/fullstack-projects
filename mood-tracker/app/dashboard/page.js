6fe86b294de333f3051eeaf32bd006f09b4282a7
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-02-04 23:10:56 +0530

    intial setup and created dashboard page

---
+++ mood-tracker/app/dashboard/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Mood-Tracker ⋅ Dashboard"
+  };
+
+export default function DashBoardPage(){
+    return(
+        <main>
+            Hello world
+        </main>
+    )
+}
