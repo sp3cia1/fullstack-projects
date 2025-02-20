43ef7eec3e8ba0d5037e3f13a6c994713d0cee2e
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-02-20 23:53:01 +0530

    minor fixes

--- mood-tracker/app/layout.js
+++ mood-tracker/app/layout.js
@@ -1,5 +1,6 @@
 import { Fugaz_One,Open_Sans } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const opensans = Open_Sans({ subsets: ["latin"] });
 
@@ -15,7 +16,9 @@ export const metadata = {
 
 const header = (
   <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
-    <h1 className={'text-lg sm:text-3xl textGradient ' + fugaz.className}>Mood-Tracker</h1>
+    <Link href={'/'}>
+      <h1 className={'text-lg sm:text-3xl textGradient ' + fugaz.className}>Mood-Tracker</h1>
+    </Link>
     <div className="flex items-center justify-between">
       PLACEHOLDER
     </div>
